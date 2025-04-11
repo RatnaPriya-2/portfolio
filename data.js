a432064aa6560b95fe08491628d13f458836b390
RatnaPriya-2 <ratnapriya.t@gmail.com>
2025-04-11 18:39:19 +0530

    Mostly done

---
+++ data.js
@@ -0,0 +1,240 @@
+export const projects = [
+  {
+    id: 1,
+    image: "./assets/periodictable.png",
+    title: "Periodic Table Explorer",
+    description:
+      "Interactive periodic table where you can click on elements to learn their details.",
+    techStack: ["./assets/html-5.png", "./assets/css-3.png", "./assets/js.png"],
+    links: {
+      liveDemo: "https://periodic22-table.netlify.app/",
+      code: "https://github.com/RatnaPriya-2/periodic-table",
+    },
+  },
+  {
+    id: 2,
+    image: "./assets/passwordgenerator.png",
+    title: "Password Generator",
+    description: "Generates a strong password.",
+    techStack: ["./assets/html-5.png", "./assets/css-3.png", "./assets/js.png"],
+    links: {
+      liveDemo: "https://password2-generator.netlify.app/",
+      code: "https://github.com/RatnaPriya-2/password-generator",
+    },
+  },
+  {
+    id: 3,
+    image: "./assets/weather.png",
+    title: "Weather App",
+    description: "Fetches weather details when searched with a city.",
+    techStack: ["./assets/html-5.png", "./assets/css-3.png", "./assets/js.png"],
+    links: {
+      liveDemo: "https://weather2-appjs.netlify.app/",
+      code: "https://github.com/RatnaPriya-2/weatherApp",
+    },
+  },
+  {
+    id: 4,
+    image: "./assets/datetime.png",
+    title: "Date-Time App",
+    description: "Displays the current date, time, day, and year.",
+    techStack: ["./assets/html-5.png", "./assets/css-3.png", "./assets/js.png"],
+    links: {
+      liveDemo: "https://mini2-date-time-app.netlify.app/",
+      code: "https://github.com/RatnaPriya-2/Mini-calendar",
+    },
+  },
+  {
+    id: 5,
+    image: "./assets/calculator.png",
+    title: "Simple Calculator",
+    description:
+      "A basic calculator that performs arithmetic operations like addition, subtraction, multiplication, and division.",
+    techStack: ["./assets/html-5.png", "./assets/css-3.png", "./assets/js.png"],
+    links: {
+      liveDemo: "https://calculator2-app.netlify.app/",
+      code: "https://github.com/RatnaPriya-2/calculator-final",
+    },
+  },
+  {
+    id: 6,
+    image: "./assets/grocerybud.png",
+    title: "Grocery Bud",
+    description:
+      "Manage your grocery list effortlessly—add, update, and remove items seamlessly.",
+    techStack: ["./assets/html-5.png", "./assets/css-3.png", "./assets/js.png"],
+    links: {
+      liveDemo: "https://grocery2-bud.netlify.app/",
+      code: "https://github.com/RatnaPriya-2/grocery-list",
+    },
+  },
+
+  {
+    id: 7,
+    image: "./assets/recipes.png",
+    title: "Recipe Finder",
+    description:
+      "Search for recipes and get detailed instructions on how to cook them.",
+    techStack: ["./assets/html-5.png", "./assets/css-3.png", "./assets/js.png"],
+    links: {
+      liveDemo: "https://recipe2-finder-app.netlify.app/",
+      code: "https://github.com/RatnaPriya-2/Recipe-app",
+    },
+  },
+  {
+    id: 8,
+    image: "./assets/mathforkids.png",
+    title: "Math 4 Kids",
+    description:
+      "A fun app for kids to practice addition, subtraction, multiplication, and division.",
+    techStack: ["./assets/html-5.png", "./assets/css-3.png", "./assets/js.png"],
+    links: {
+      liveDemo: "https://math2-for-kids.netlify.app/",
+      code: "https://github.com/RatnaPriya-2/math-for-kids",
+    },
+  },
+  {
+    id: 9,
+    image: "./assets/countries.png",
+    title: "Rest Countries API",
+    description:
+      "Discover country details including flags, population, and region. Search and filter countries by continent.",
+    techStack: ["./assets/html-5.png", "./assets/css-3.png", "./assets/js.png"],
+    links: {
+      liveDemo: "https://rest2-countries-api.netlify.app/",
+      code: "https://github.com/RatnaPriya-2/rest-countries-api-with-color-theme-switcher-master",
+    },
+  },
+];
+
+export const reactProjects = [
+  {
+    id: 1,
+    image: "./assets/textutils.png",
+    title: "Text Utils",
+    description:
+      "A web app to manipulate and analyze text, including case conversion, word count.",
+    techStack: ["./assets/react.png", "./assets/tailwind.png"],
+    links: {
+      liveDemo: "https://text2-utils.netlify.app/",
+      code: "https://github.com/RatnaPriya-2/textutils",
+    },
+  },
+  {
+    id: 2,
+    image: "./assets/cookbookpro.png",
+    title: "CookBook Pro",
+    description:
+      "Search, view, and save your favorite recipes with video tutorials for a seamless cooking experience.",
+    techStack: ["./assets/react.png", "./assets/css-3.png"],
+    links: {
+      liveDemo: "https://recipe2-finder.netlify.app/",
+      code: "https://github.com/RatnaPriya-2/recipe-finder",
+    },
+  },
+  {
+    id: 3,
+    image: "./assets/colorpalettegenerator.png",
+    title: "Color Palette Generator",
+    description: "Generates different shades of a given hex color.",
+    techStack: ["./assets/react.png", "./assets/css-3.png"],
+    links: {
+      liveDemo: "https://color2-palette-generator.netlify.app/",
+      code: "https://github.com/RatnaPriya-2/color-palette-generator",
+    },
+  },
+  {
+    id: 4,
+    image: "./assets/cocktail.png",
+    title: "Cocktail DB",
+    description:
+      "A cocktail search app to find drink recipes, ingredients, and instructions using TheCocktailDB API.",
+    techStack: ["./assets/react.png", "./assets/css-3.png"],
+    links: {
+      liveDemo: "https://cocktail2-db.netlify.app/",
+      code: "https://github.com/RatnaPriya-2/cocktaildb",
+    },
+  },
+];
+
+export const bigReactProjects = [
+  {
+    id: 1,
+    name: "Trendikart",
+    image: "./assets/trendikart.png",
+    title: "Trendikart - Modern E-commerce",
+    description:
+      "A modern and responsive e-commerce website built with React for seamless online shopping.",
+    features: [
+      "Login feature using Firebase and Firestore",
+      "Product listing with filters based on price and category",
+      "Shopping cart and checkout functionality",
+      "Integrated payment gateway with Razorpay",
+      "Responsive design with optimized layouts for mobile and desktop",
+      "User-friendly navigation and product search",
+      "Context API for state management",
+      "Custom hooks for pagination and cart logic",
+    ],
+    techStack: [
+      "./assets/react.png",
+      "./assets/css-3.png",
+      "./assets/firebase.png",
+      "./assets/razorpay.png",
+    ],
+    links: {
+      liveDemo: "https://trendikart.netlify.app/",
+      code: "https://github.com/RatnaPriya-2/ecommerce",
+    },
+    technologies: {
+      frontend: [
+        "React",
+        "React Router",
+        "Context API",
+        "Razorpay",
+        "Firebase",
+        "Firestore",
+      ],
+      styling: ["CSS", "Responsive Design", "Media Queries"],
+      stateManagement: ["React Context", "Custom Hooks"],
+      payment: ["Razorpay"],
+      authentication: ["Firebase", "Firestore"],
+    },
+  },
+  {
+    id: 2,
+    name: "Netflix-Clone",
+    image: "./assets/netflix.png",
+    title: "Netflix Clone - Streaming Platform UI",
+    description:
+      "A fully responsive Netflix-inspired movie streaming interface using React, featuring TMDB integration for real-time content browsing.",
+    features: [
+      "Sign Up and SIgn In feature using Firebase and Firestore",
+      "User-friendly movie and TV show browsing",
+      "Dynamic movie data fetched from TMDB API",
+      "Language-based and Genre based movie filtering",
+      "Watch page with trailer support",
+      "Responsive design for all screen sizes",
+      "Reusable components and clean code structure",
+    ],
+    techStack: [
+      "./assets/react.png",
+      "./assets/css-3.png",
+      "./assets/firebase.png",
+    ],
+    links: {
+      liveDemo: "https://fnc-tmdb-api.netlify.app/",
+      code: "https://github.com/RatnaPriya-2/final-netflix-clone",
+    },
+    technologies: {
+      frontend: [
+        "React",
+        "React Router",
+        "Context API",
+        "Firebase",
+        "Firestore",
+      ],
+      styling: ["CSS", "Responsive Design", "Media Queries"],
+      authentication: ["Firebase", "Firestore"],
+    },
+  },
+];
---
+++ script.js
@@ -0,0 +1,121 @@
+import { projects, reactProjects, bigReactProjects } from "./data.js";
+
+window.addEventListener("pageshow", function () {
+  document.querySelector(".contactForm")?.reset();
+});
+
+const hamburger = document.querySelector(".hamburger");
+const closeBtn = document.querySelector(".close-btn");
+const leftSection = document.querySelector(".left-section");
+
+hamburger.addEventListener("click", () => {
+  leftSection.classList.toggle("active");
+});
+closeBtn.addEventListener("click", () => {
+  leftSection.classList.toggle("active");
+});
+
+let liItems = document.querySelectorAll(".left-section ul li");
+
+liItems.forEach((item) => {
+  let path = window.location.pathname;
+  let anchor = item.querySelector("a").getAttribute("href");
+
+  if (anchor === path) {
+    item.classList.add("active");
+  }
+
+  if (
+    (anchor === "/" && path === "/") ||
+    (anchor === "/" && path === "/index.html")
+  ) {
+    item.classList.add("active");
+  }
+});
+
+let smallProjectsGrid = document.querySelector(".small-projects");
+let bigProjectsGrid = document.querySelector(".big-projects");
+
+let projectBtns = document.querySelector(".project-btns");
+
+const createCard = (project) => {
+  let projectCard = document.createElement("div");
+  projectCard.classList.add("project-card");
+  projectCard.innerHTML = `<div class="project-img"><img
+                  src=${project.image}
+                  alt=${project.title}
+                /></div>
+                <div class="card-content">
+                  <h3>${project.title}</h3>
+                  <p>${project.description}</p>
+                  <div class="tags">
+                    ${project.techStack
+                      .map((tech) => `<img src=${tech} alt="tech-icon"/>`)
+                      .join("")}
+                  </div>
+                  <div class="project-links">
+                    <button>
+                      <a
+                        href=${project.links.liveDemo}
+                        target="_blank"
+                        >Live Demo</a
+                      >
+                    </button>
+                    <button>
+                      <a
+                        href=${project.links.code}
+                        target="_blank"
+                        >Code</a
+                      >
+                    </button>
+                  </div>
+                </div>
+              `;
+
+  smallProjectsGrid.appendChild(projectCard);
+};
+
+const createProjectCard = (project) => {
+  let projectContainer = document.createElement("div");
+  projectContainer.classList.add("big-project-container");
+  projectContainer.innerHTML = `<img src="${project.image}" alt="${
+    project.title
+  }" class="big-project-image"/>
+        <div class="big-project-info">
+          <h3 class="big-project-title">${project.title}</h3>
+          <p class="big-project-description">${project.description}</p>
+          <ul class="big-project-features">${project.features
+            .map((feature) => `<li>${feature}</li>`)
+            .join("")}</ul>
+          <div class="tech-stack">
+            ${project.techStack
+              .map(
+                (tech) =>
+                  `<img src="${tech}" alt="tech icon" class="tech-icon">`
+              )
+              .join("")}
+          </div>
+          <div class="big-project-links">
+          <button> <a href="${
+            project.links.liveDemo
+          }" target="_blank">Live Demo</a></button>
+           <button><a href="${
+             project.links.code
+           }" target="_blank">Code</a></button>
+            
+          </div>
+        </div>
+      `;
+  bigProjectsGrid.appendChild(projectContainer);
+};
+
+projectBtns.addEventListener("click", (e) => {
+  let tech = e.target.innerText;
+  smallProjectsGrid.innerHTML = "";
+  (tech.includes("JavaScript") ? projects : reactProjects).forEach(
+    (project) => {
+      createCard(project);
+    }
+  );
+  bigReactProjects.forEach((project) => createProjectCard(project));
+});
